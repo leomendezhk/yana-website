9fd41d4b0d5ccf57c4fca6551353008904ca6db2
leomendezhk <leomendezhk3@gmail.com>
2022-11-20 11:32:19 +0800

    sized porfolio

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@
 
 const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
+const smallheaderEl = document.querySelector('.small-header');
 
 btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
+btnNavEl.addEventListener('click', function () {
+  smallheaderEl.classList.toggle('nav-open');
+});
+
+//////////////////////////////////////////////////////////
